7a95327d0cda4de16d5dbba717c0d5552513e865
xiangshifu <2399770210@qq.com>
2019-07-30 18:01:04 +0800

    添加了约瑟夫（环形列表）

---
+++ src/LinkeList/Boy.java
@@ -0,0 +1,26 @@
+package LinkeList;
+
+public class Boy {
+    private int no; //编号
+    private Boy next;//指向下一个节点，默认为null
+
+    public Boy(int no) {
+        this.no = no;
+    }
+
+    public int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+
+    public Boy getNext() {
+        return next;
+    }
+
+    public void setNext(Boy next) {
+        this.next = next;
+    }
+}
---
+++ src/LinkeList/TestJoephuList.java
@@ -0,0 +1,11 @@
+package LinkeList;
+
+public class TestJoephuList {
+    public static void main(String args[]){
+        JoephuList joephuList  = new JoephuList();
+        joephuList.addBoy(5);
+        joephuList.showBoy();
+//        joephuList.countBoy(1,2,5);
+        joephuList.countBoy2(1,2,5);
+    }
+}
